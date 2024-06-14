917c38097d1aa3a3bfe7143002e59dd7c0f37408
luthfiahmad12 <luthfiafif625@gmail.com>
2024-06-15 01:41:02 +0700

    commit feat[smooth scroll]

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,8 @@ document.getElementById("menu-btn").addEventListener("click", function () {
   var menu = document.getElementById("dropdown");
   menu.classList.toggle("hidden");
 });
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 900,
+  speedAsDuration: true,
+});
